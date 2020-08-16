ee03c8666f6428a3cd6a3aeb29ea7218ec03b388
Conor <conortunc@gmail.com>
2020-08-16 14:17:05 -0400

    log in page?

---
+++ client/client.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function () {
+    $('#loginButton').click(function () {
+        var username = $("#login");
+        alert('good job' + username+', you logged in');
+    });
+
+
+
+});
