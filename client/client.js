767a5e61ced4668eaa3bf5e610abdb78e325cff1
Conor <conortunc@gmail.com>
2020-08-17 17:51:35 -0400

    added strophe

--- client/client.js
+++ client/client.js
@@ -1,4 +1,8 @@
-
+var client={
+    connection:null,
+    http_bind:"http://3.129.13.151:5280/http",
+    name:null
+}
 
 $(document).ready(function () {
 
@@ -17,5 +21,27 @@ $(document).ready(function () {
 //This is the code that is called when the login event is fired.
 //the data object has everything we stored in the previous code up above
 $(document).on('login', function (event, data) {
-    alert('good job' + data.jid +', you logged in');
+    console.log('login fired');
+    client.name=data.jid;
+    client.connection = new Strophe.Connection(client.http_bind);
+    client.connection.connect(data.jid, data.password, function (status) {
+        if(status===Strophe.Status.CONNECTED) {
+            $(document).trigger('connected');
+        } else if (status === Strophe.Status.DISCONNECTED){
+            $(document).trigger('disconnected');
+        }
+
+    });
+});
+
+$(document).on('connected', function () {
+    console.log('connected fired');
+    alert('good job ' + client.name +', you logged in');
+    client.connection.disconnect();
+});
+
+
+$(document).on('disconnected', function () {
+    console.log('dc fired')
+    alert('disconnected');
 });
