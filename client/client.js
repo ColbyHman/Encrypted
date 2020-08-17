1319b5f19f8798b6c1127fab3290e6180ab4526c
Conor <conortunc@gmail.com>
2020-08-17 15:18:35 -0400

    made login use events

--- client/client.js
+++ client/client.js
@@ -1,11 +1,21 @@
 
 
 $(document).ready(function () {
+
+    //We bind the loging button to fire the login event.
+    //The login event code can be found further down in the file.
+    //Using events helps to keep things more organized by compartmentalizing the code.
     $('#loginButton').click(function () {
-        var username = $("#login");
-        alert('good job' + username+', you logged in');
+        $(document).trigger('login',{
+            jid: $("#jid").val(),
+            password: $("#password").val()
+        });
     });
 
+});
 
-
+//This is the code that is called when the login event is fired.
+//the data object has everything we stored in the previous code up above
+$(document).on('login', function (event, data) {
+    alert('good job' + data.jid +', you logged in');
 });
